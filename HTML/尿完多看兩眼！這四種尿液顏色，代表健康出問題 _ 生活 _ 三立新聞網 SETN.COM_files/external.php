265eb96ef2ec3c8aa9d86ec2d48c04eb36aaa416
jash-git <jash.liao@gmail.com>
2018-11-28 09:15:41 +0800

    create

---
+++ HTML/尿完多看兩眼！這四種尿液顏色，代表健康出問題 _ 生活 _ 三立新聞網 SETN.COM_files/external.php
@@ -0,0 +1,35 @@
+/* in-freq, hit 2 tw */
+if (ONEAD_get_response[0] !== undefined && typeof ONEAD_get_response[0] == 'function'){
+    ONEAD_get_response[0]({
+      "guid": "0dfd8a97-e0af-11e8-9aed-0242ac120003"
+    , "pid": 1168772
+    , "uid": 1000052
+    , "adv_id": 20
+    , "cm_custom_id": ""
+    , "camp_id": 10775
+    , "ct_id": 2
+    , "pm_id": 8856
+    , "cm": "false"
+    , "play_mode": "inread"
+    , "run": "2:429488-417317 [400 401 402 404 407 410 501 502 503 504 505 506 507 508 509 510 511 512 513 531 532 533 534 535 536 537 538 539 540 541 542 543 544 545 546 551 639 840 930 931 942 943]"
+    , "tout": 3000
+    , "is_cprp": "true"
+    , "is_vast": "false"
+    , "is_custom_pid": "false"
+    , "force_backend": "false"
+    , "term_id":  "-1" 
+    , "player_type": "HTML5_IS_DF"
+    , "isip_index": 0
+    , "width": 640
+    , "height": 390
+    , "stage_width": 638
+    , "stage_height": 388
+    , "base_url": "https://onead.onevision.com.tw/"
+    , "iframe_url": ""
+    , "tracking": "//onead.onevision.com.tw/v2/async?_hash=15670462489808019256&action=&c1=1168772&c2=inread&clicklabel=[CLICKLABEL]&cmpid=10775&ctid=2&dt=1543367193&errorcode=[ERRORCODE]&errordesc=[ERRORDESC]&errormsg=[ERRORMSG]&event=[EVENT]&gc=2&host=https%3A%2F%2Fonead.onevision.com.tw%2F&ix=0&p1=1000052&p10=101%2C601%2C602%2C606%2C610%2C612%2C613%2C904%2C905%2C908%2C913%2C915&p11=5&p12=5%2C7&p13=13%2C3%2C12%2C1%2C2%2C8%2C11%2C4%2C9%2C6&p14=&p15=&p2=0dfd8a97-e0af-11e8-9aed-0242ac120003&p3=https%3A%2F%2Fwww.setn.com%2FNews.aspx%3FNewsID%3D461819&p4=%E5%B0%BF%E5%AE%8C%E5%A4%9A%E7%9C%8B%E5%85%A9%E7%9C%BC%EF%BC%81%E9%80%99%E5%9B%9B%E7%A8%AE%E5%B0%BF%E6%B6%B2%E9%A1%8F%E8%89%B2%EF%BC%8C%E4%BB%A3%E8%A1%A8%E5%81%A5%E5%BA%B7%E5%87%BA%E5%95%8F%E9%A1%8C+%7C+%E7%94%9F%E6%B4%BB+%7C+%E4%B8%89%E7%AB%8B%E6%96%B0%E8%81%9E%E7%B6%B2+SETN.COM&p5=&p6=27c103bdebcb11f18bb0a7133d668607%3A110.50.171.217&p7=-1&p8=602%2C613%2C610%2C612%2C606%2C601%2C101%2C908&p9=1012%2C15%2C1101%2C1109%2C1302%2C615%2C9%2C1305%2C907%2C103%2C817%2C201%2C1107%2C602%2C604%2C224%2C3%2C413%2C619%2C1111%2C616%2C102%2C713%2C613%2C804%2C229%2C1102%2C716%2C816%2C1%2C1201%2C618%2C11%2C510%2C1004%2C711%2C610%2C113%2C105%2C1418%2C810%2C418%2C1507%2C230%2C1308%2C107%2C1016%2C203%2C2%2C109%2C702%2C815%2C1202%2C1110%2C1011%2C110%2C811%2C7%2C714%2C609%2C13%2C1422%2C1017%2C1310%2C701%2C1001%2C406%2C1002%2C612%2C1015%2C606%2C607%2C605%2C1203%2C212%2C1419%2C14%2C1421%2C315%2C12%2C614%2C611%2C1304%2C601%2C222%2C1301%2C1018%2C818%2C813%2C223%2C621%2C1113%2C5%2C8%2C1006%2C809%2C4%2C803%2C1204%2C1007%2C504%2C501%2C106%2C608%2C101%2C805%2C1013%2C420%2C221%2C228%2C10%2C806%2C210%2C617%2C908%2C603%2C1420%2C620%2C211%2C1009%2C808%2C6&plaid=8856&pms=inread&vol=0.05"
+    , "l_url": "https://onead.onevision.com.tw/cprp/xml/1168772/l.xml?pid=1168772&campid=10775&uid=1000052&guid=0dfd8a97-e0af-11e8-9aed-0242ac120003"
+    });
+
+}
+/* [server]/client */
+
